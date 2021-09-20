447f99ead5f7caf00affe249d6ef142a31655aff
kbjude <kbjude@gmail.com>
2021-09-20 15:00:22 +0300

    Working with arrays

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,10 +6,19 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            double x = 45.9;
+            var x = 45.9;
             int t = 4;
-
             var result = x + t;
+
+            var numbers = new double[3];
+
+            numbers[0] = 24.90;
+            numbers[1] = 56.5;
+            numbers[2] = 77.9;
+
+            var total = numbers[0] + numbers[1] + numbers[2];
+
+            Console.WriteLine($"This is my total {total}");
 
             Console.WriteLine(result);
 
