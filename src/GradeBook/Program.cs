27a36cf43ed5775c222e8eb04b6e8a5fe9327bcb
kbjude <kbjude@gmail.com>
2021-09-21 00:02:16 +0300

    iterations

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,13 +10,19 @@ namespace GradeBook
             int t = 4;
             var result = x + t;
 
-            var numbers = new double[3];
+            var numbers = new double[] {12.4, 56.8, 56.3, 67.8, 21.9 };
 
-            numbers[0] = 24.90;
-            numbers[1] = 56.5;
-            numbers[2] = 77.9;
+            var total = 0.0;
 
-            var total = numbers[0] + numbers[1] + numbers[2];
+            foreach(var number in numbers)
+            {
+                total += number;
+            }
+
+            //numbers[0] = 24.90;
+            //numbers[1] = 56.5;
+            //numbers[2] = 77.9;
+            //var total = numbers[0] + numbers[1] + numbers[2];
 
             Console.WriteLine($"This is my total {total}");
 
