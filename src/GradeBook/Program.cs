3c258f6f4884268a67b083b9eacb059684c3130d
kbjude <kbjude@gmail.com>
2021-09-20 14:38:50 +0300

    Mathmatical expressions

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,13 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            double x = 45.9;
+            int t = 4;
+
+            var result = x + t;
+
+            Console.WriteLine(result);
+
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello World {args[0]}!");
