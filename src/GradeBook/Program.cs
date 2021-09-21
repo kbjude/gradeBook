570fd32bf8d52af6dd8f88dd332c0001e793332b
kbjude <kbjude@gmail.com>
2021-09-21 08:44:43 +0300

    Refactor to focus on the grade book

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,10 +7,6 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var x = 45.9;
-            int t = 4;
-            var result = x + t;
-
             var grades = new List<double>() { 12.4, 56.8, 56.3, 67.8, 21.9 };
             grades.Add(56.1);
 
@@ -23,20 +19,10 @@ namespace GradeBook
                 total += number;
             }
 
-            result = total / grades.Count;
+            var result = total / grades.Count;
 
 
             Console.WriteLine($"This is my average {result:N1}");
-
-
-            if(args.Length > 0)
-            {
-                Console.WriteLine($"Hello World {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine("Hello no Name!");
-            }
         }
     }
 }
