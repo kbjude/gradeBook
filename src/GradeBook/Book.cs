6f46cc22447a0be20fa374ce5317418a2bdb5425
kbjude <kbjude@gmail.com>
2021-09-22 10:47:03 +0300

    Working with Encapsulation

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,15 +1,43 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Book
     {
+        public Book(string name)
+            {
+            this.name = name;
+            grades = new List<double>();
+            }
         public void AddGrade(double grade)
         {
             grades.Add(grade); 
         }
 
-        List <double> grades = new List<double>() { };
+        public void ShowStatistics()
+        {
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            var total = 0.0;
+
+            foreach (var number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+
+                total += number;
+            }
+
+            var result = total / grades.Count;
+
+
+            Console.WriteLine($"This is my average {result:N1}");
+            Console.WriteLine($"This is the lowest grade {lowGrade:N1}");
+            Console.WriteLine($"This is the highest grade {highGrade:N1}");
+        }
+        List <double> grades;
+        string name;
     }
 
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -5,27 +5,20 @@ namespace GradeBook
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            var book = new Book();
+            var book = new Book("Jude");
             book.AddGrade(89.1);
+            book.AddGrade(56.7);
+            book.AddGrade(27.7);
+            book.AddGrade(78.98);
+            book.ShowStatistics();
 
-            var grades = new List<double>() { 12.4, 56.8, 56.3, 67.8, 21.9 };
-            grades.Add(56.1);
+            //var grades = new List<double>() { 12.4, 56.8, 56.3, 67.8, 21.9 };
+            //grades.Add(56.1);
 
             //var numbers = new double[] {12.4, 56.8, 56.3, 67.8, 21.9 };
-
-            var total = 0.0;
-
-            foreach(var number in grades)
-            {
-                total += number;
-            }
-
-            var result = total / grades.Count;
-
-
-            Console.WriteLine($"This is my average {result:N1}");
         }
     }
 }
