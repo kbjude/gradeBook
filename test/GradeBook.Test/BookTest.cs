63a268952622710ceb40078a32b89ed70abb5656
kbjude <kbjude@gmail.com>
2021-09-26 00:39:56 +0300

    Working with pointers and tests

--- test/GradeBook.Test/BookTest.cs
+++ test/GradeBook.Test/BookTest.cs
@@ -6,7 +6,7 @@ namespace GradeBook.Test
     public class BookTest
     {
         [Fact]
-        public void Test1()
+        public void BookCalculatesAverageGrade()
         {
             //arrange
             var book = new Book("");
--- test/GradeBook.Test/TypeTest.cs
+++ test/GradeBook.Test/TypeTest.cs
@@ -11,10 +11,48 @@ namespace GradeBook.Test
             //arrange
 
             var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+
+            //act
+
+            //assert
+
+        }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            //arrange
+
+            var book1 = GetBook("Book 1");
             var book2 = GetBook("Book 2");
 
             Assert.Equal("Book 1", book1.Name);
             Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2);
+            //act
+
+            //assert
+
+        }
+
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            //arrange
+
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
             //act
 
             //assert
