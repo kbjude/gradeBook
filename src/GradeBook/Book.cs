76105b56960b6830a8cc6c0f5b5412ab2e0ae5d3
kbjude <kbjude@gmail.com>
2021-09-27 17:24:57 +0300

    Add case statement

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -5,6 +5,36 @@ namespace GradeBook
 {
     public class Book
     {
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+
+                    AddGrade(90);
+                    break;
+
+                case 'B':
+
+                    AddGrade(80);
+                    break;
+
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                case 'D':
+                        AddGrade(60);
+                        break;
+
+                default:
+                    
+                       AddGrade(0);
+                       break;
+                    
+            }
+        }
+
         public Book(string name)
             {
             Name = name;
@@ -12,7 +42,14 @@ namespace GradeBook
             }
         public void AddGrade(double grade)
         {
-            grades.Add(grade); 
+            if (grade <=100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Value")
+            }
         }
 
         public Statistics GetStatistics()
