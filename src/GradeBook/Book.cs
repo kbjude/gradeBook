41c28e16c696cc2fb8febf22f98065639c68b1f7
kbjude <kbjude@gmail.com>
2021-09-29 12:39:03 +0300

    Working final refactored project

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -65,7 +65,18 @@ namespace GradeBook
 
         public override Statistics GetStatistics()
         {
-            throw new NotImplementedException();
+            var result = new Statistics();
+            using(var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = reader.ReadLine();
+                while(line != null)
+                {
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+            }
+            return result;
         }
     }
 
@@ -132,40 +143,15 @@ namespace GradeBook
         public override Statistics GetStatistics()
         {
             var result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
 
-            foreach (var grade in grades)
+
+            for(var index = 0; index < grades.Count; index +=1)
             {
-                result.High = Math.Max(grade, result.High);
-                result.Low = Math.Min(grade, result.Low);
-                result.Average += grade;
+                result.Add(grades[index]);
             }
 
-            result.Average /= grades.Count;
 
-            switch(result.Average)
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-
-                case var d when d >= 60.0:
-                    result.Letter = 'C';
-                    break;
-
-                case var d when d >= 90.0:
-                    result.Letter = 'D';
-                    break;
-                default:
-                    result.Letter = 'F';
-                    break;
-            }
+ 
 
             return result;
         }
--- src/GradeBook/Statistics.cs
+++ src/GradeBook/Statistics.cs
@@ -3,9 +3,56 @@ namespace GradeBook
 {
     public class Statistics
     {
-        public double Average;
+        public double Average
+        {
+            get
+            {
+                return Sum / Count;
+            }
+        }
         public double High;
         public double Low;
-        public char Letter;
+        public char Letter
+        {
+            get
+            {
+                switch (Average)
+                {
+                    case var d when d >= 90.0:
+                        return 'A';
+
+                    case var d when d >= 80.0:
+                        return 'B';
+
+                    case var d when d >= 60.0:
+                        return 'C';
+
+                    case var d when d >= 90.0:
+                        return 'D';
+
+                    default:
+                        return 'F';
+
+                }
+            }
+        }
+        public double Sum;
+        public int Count;
+
+        public void Add(double number)
+        {
+            Sum += number;
+            Count += 1;
+            High = Math.Max(number, High);
+            Low = Math.Min(number, Low);
+        }
+
+        public Statistics()
+        {
+            High = double.MinValue;
+            Low = double.MaxValue;
+            Sum = 0.0;
+            Count = 0;
+        }
     }
 }
